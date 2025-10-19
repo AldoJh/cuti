5c2c72b7f6f4cbc3713a9cc786a65ba0380ecac5
Aldo <aldo.210170184@mhs.unimal.ac.id>
2025-10-19 15:03:11 +0700

    revisi Model

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -53,7 +53,7 @@ class DashboardController extends Controller
 
     foreach ($users as $user) {
         // Total cuti tahunan yang sudah diambil tahun ini
-        $totalTahunan = \App\Models\pengajuan_cuti::where('user_id', $user->id)
+        $totalTahunan = \App\Models\Pengajuan_Cuti::where('user_id', $user->id)
             ->where('jenis_cuti', 'tahunan')
             ->whereYear('tanggal_mulai', $tahunIni)
             ->sum(\DB::raw('DATEDIFF(tanggal_selesai, tanggal_mulai) + 1'));
